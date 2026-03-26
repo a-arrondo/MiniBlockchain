b2dba678c97c50c9a6063e5bdd21bbe37780c778
a-arrondo <a.arrondo.git@gmail.com>
2026-03-26 20:00:22 +0100

    main changed to test blockchain functions

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
 
 from pprint import pprint
 
-from schemas import Block
+from schemas import Blockchain, Transaction
 
 def main():
-    genesis = Block(0, "Alice", "Bob", 10, None)
-    pprint(genesis)
+    transactions_log = [
+        {
+            "sender": "Alice",
+            "receiver": "Bob",
+            "amount": 10.0
+        },
+        {
+            "sender": "Bob",
+            "receiver": "Alice",
+            "amount": 5.0
+        },
+        {
+            "sender": "Bob",
+            "receiver": "Alice",
+            "amount": 2.3
+        },
+        {
+            "sender": "Bob",
+            "receiver": "Alice",
+            "amount": 2.7
+        }
+    ]
+
+    blockchain = Blockchain()
+    print(f"Validation: {blockchain.validate_chain()}\n")
+    for i, transaction in enumerate(transactions_log):
+        print(f"# Transaction {i}")
+        blockchain.pending_transactions.append(
+            Transaction(
+                sender=transaction["sender"],
+                receiver=transaction["receiver"],
+                amount=transaction["amount"]
+            )
+        )
+        blockchain.create_new_block()
+        pprint(blockchain)
+        print(f"Validation: {blockchain.validate_chain()}\n")
+        
 
 if __name__ == "__main__":
     main()
